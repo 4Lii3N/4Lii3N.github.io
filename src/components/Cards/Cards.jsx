2ffb898f237fcd28a89187b7cb993acb6ca88c09
Keenan Blunt <keenan.blunt@syzygy.de>
2024-02-15 20:45:49 +0100

    add placeholder imgs

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,14 +3,16 @@ import './Cards.scss';
 
 import Card from '@components/Card/Card.jsx';
 import card1_bg from '@assets/card1_bg.png';
+import card2_bg from '@assets/card2_bg.jpg';
+import card3_bg from '@assets/card3_bg.jpg';
 
 
 const Cards = () => {
   return (
     <div className="cards">
       <Card title="Card 1" description="This is card 1" index="1" img={card1_bg} />
-      <Card title="Card 2" description="This is card 2" index="2" />
-      <Card title="Card 3" description="This is card 3" index="3" />
+      <Card title="Card 2" description="This is card 2" index="2" img={card2_bg} />
+      <Card title="Card 3" description="This is card 3" index="3" img={card3_bg} />
     </div>
   );
 }
